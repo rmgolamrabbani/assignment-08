aa28e87d3082634874e8cfa39ff25589bf33ff50
G. <rmgolamrabbani@gmail.com>
2026-05-01 20:30:43 +0600

    fix some bug

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h2>পেজটি খুঁজে পাওয়া যায়নি!</h2>
-      <p>দুঃখিত, আপনি যে ইউআরএলটি খুঁজছেন তা আমাদের কাছে নেই।</p>
+      <p>দুঃখিত, আপনি যে ইউআরএলটি খুঁজছেন তা নেই।</p>
       <Link href="/" className='border border-gray-600 rounded-lg px-4 py-2 text-gray-300 hover:bg-gray-800 transition'>
         হোম পেজে ফিরে যান
       </Link>
