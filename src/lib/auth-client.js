0e995102d40f6321e6fbcee0c55a15fb5afedb3e
G. <rmgolamrabbani@gmail.com>
2026-05-02 10:46:52 +0600

    change a file path

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    
-    baseURL: "https://assignment-08-flame.vercel.app"
-})
 
+export const authClient = createAuthClient({
+   
+    baseURL: process.env.NEXT_PUBLIC_APP_URL || typeof window !== 'undefined' ? window.location.origin : "http://localhost:3000"
+})
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("qurbani_hat");
+const db = client.db("qurbani-hat");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
